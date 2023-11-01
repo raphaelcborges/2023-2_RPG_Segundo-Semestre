dc1666a9840b5046d4fb3fa629d74b67890733b8
vaahazz <143141341+vaahazz@users.noreply.github.com>
2023-10-31 23:49:42 -0300

    Update Oinimigo.hpp

--- Oinimigo.hpp
+++ Oinimigo.hpp
@@ -1,1 +1,24 @@
+#ifndef OINIMIGO_HPP
+#define OINIMIGO_HPP
 
+#include <iostream>
+using namespace std;
+
+class OInimigo {
+protected:
+    string _nome;
+    int _vida;
+    int _ataque;
+
+public:
+    OInimigo(string nome);
+    virtual ~OInimigo() {}
+    void recebeDano(int dano);
+    bool estaVivo();
+    void printInfo();
+    virtual int ataca() = 0;
+    virtual void falar() = 0;
+    int numeroAleatorio();
+};
+
+#endif
