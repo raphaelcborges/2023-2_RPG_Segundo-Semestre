1f4b24c72f18fcce83b9d64fea43a395d3cc71a5
kayquefb <149022324+kayquefb@users.noreply.github.com>
2023-11-01 00:00:06 -0300

    Update Batalha.hpp

--- Batalha.hpp
+++ Batalha.hpp
@@ -1,1 +1,19 @@
-B
+#ifndef BATALHA_HPP
+#define BATALHA_HPP
+
+#include "Personagem.hpp"
+#include "OInimigo.hpp"
+
+class Batalha {
+    Personagem* personagem;
+    OInimigo* inimigo;
+
+public:
+    Batalha(Personagem* personagem, OInimigo* inimigo);
+    void executaTurno(int escolha);
+    bool terminou();
+    void executaTurnoInimigo();
+};
+
+#endif
+
