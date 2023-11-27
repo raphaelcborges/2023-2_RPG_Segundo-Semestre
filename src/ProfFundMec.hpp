998d73b34bd39113ef6c28a3146c5b76afd96249
kayquefb <149022324+kayquefb@users.noreply.github.com>
2023-11-27 10:53:08 -0300

    Create ProfFundMec.hpp

---
+++ src/ProfFundMec.hpp
@@ -0,0 +1,24 @@
+ProfFundMec.cpp
+#include "ProfFundMec.hpp"
+
+ProfFundMec::ProfFundMec(string nome) : Inimigo(nome) {
+    this->_vida = 40;
+    this->_ataque = 10;
+}
+
+int ProfFundMec::ataca() {
+    return _ataque;
+}
+
+void ProfFundMec::falar() {
+    int fala = numeroAleatorio();
+    if (fala == 1) {
+        cout << "Maça de Newton!\n" << endl;
+    }
+    else if (fala == 2) {
+        cout << "Conservacao de Energia!\n" << endl;
+    }
+    else {
+        cout << "Rotacao!\n" << endl;
+    }
+}
