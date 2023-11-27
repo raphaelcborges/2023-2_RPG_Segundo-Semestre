1021eb8265319ca604de3c072de1968edbe5220a
raphaelcborges <129281871+raphaelcborges@users.noreply.github.com>
2023-11-27 06:29:48 -0300

    Update Personagem.cpp

--- src/Personagem.cpp
+++ src/Personagem.cpp
@@ -34,6 +34,49 @@ string Personagem::getNome() {
     return _nome;
 }
 
+
+void Personagem::usarItem(int indiceItem) {
+    if (indiceItem < 0 || indiceItem >= inventario.size()) {
+        return;
+    }
+
+    _vida += inventario[indiceItem].getValorDeRegeneracao();
+    if (_vida > 100) {
+        _vida = 100;
+    }
+    std::cout << "O item regenerou " << inventario[indiceItem].getValorDeRegeneracao() << " do seu NSG\n" << std::endl;
+    printInfo();
+    inventario.erase(inventario.begin() + indiceItem);
+}
+
+void Personagem::addItem(Item item) {
+    inventario.push_back(item);
+}
+
+std::vector<Item>& Personagem::getInventario() {
+    return inventario;
+}
+
+void Personagem::exibirInventario() {
+    if (inventario.empty()) {
+        std::cout << "Voce abriu sua mochila, mas ela estava vazia :( , nesse tempo o professor escreveu no quadro! Isso vai ser descontado no seu NSG!!! (aperte 1 para voltar)" << std::endl;
+        std::cout << std::endl;
+        std::string escolha;
+        while (true) {
+            std::getline(std::cin, escolha);
+            if (escolha == "1") {
+                break;
+            }
+        }
+        return;
+    }
+    std::cout << "Inventário:\n";
+    for (int i = 0; i < inventario.size(); ++i) {
+        std::cout << i << ". " << inventario[i].getNome() << " (Regenera " << inventario[i].getValorDeRegeneracao() << " de NSG)\n";
+    }
+}
+
+
 int Personagem::getVida() {
     return _vida;
 }
