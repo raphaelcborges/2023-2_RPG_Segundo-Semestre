0ef689aee283a86974d2dc48f7183cebc1db8d8c
kayquefb <149022324+kayquefb@users.noreply.github.com>
2023-12-04 21:30:03 -0300

    Update Batalha.hpp

--- Include/Batalha.hpp
+++ Include/Batalha.hpp
@@ -1,18 +1,39 @@
-Batalha.hpp
 #ifndef BATALHA_HPP
 #define BATALHA_HPP
 
 #include "Personagem.hpp"
 #include "Inimigo.hpp"
 
+/**
+ * @brief Classe que representa uma batalha entre um Personagem e um Inimigo.
+ */
 class Batalha {
-    Personagem* personagem;
-    Inimigo* inimigo;
+    Personagem* personagem; /**< Ponteiro para o Personagem. */
+    Inimigo* inimigo; /**< Ponteiro para o Inimigo. */
 
 public:
+    /**
+     * @brief Construtor da classe Batalha.
+     * @param personagem Ponteiro para o Personagem.
+     * @param inimigo Ponteiro para o Inimigo.
+     */
     Batalha(Personagem* personagem, Inimigo* inimigo);
+
+    /**
+     * @brief Executa um turno da batalha com base na escolha do jogador.
+     * @param escolha Escolha do jogador (1 ou 2).
+     */
     void executaTurno(int escolha);
+
+    /**
+     * @brief Verifica se a batalha terminou.
+     * @return true se a batalha terminou, false caso contrário.
+     */
     bool terminou();
+
+    /**
+     * @brief Executa o turno do Inimigo.
+     */
     void executaTurnoInimigo();
 };
 
