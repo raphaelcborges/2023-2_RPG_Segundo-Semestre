05085795df8090bada0bb43ef4ec545808cb249f
raphaelcborges <129281871+raphaelcborges@users.noreply.github.com>
2023-12-04 21:43:33 -0300

    Update ProfCalculo2.hpp

--- Include/ProfCalculo2.hpp
+++ Include/ProfCalculo2.hpp
@@ -3,12 +3,30 @@
 
 #include "Inimigo.hpp"
 
+/**
+ * @brief Classe que representa um Inimigo do tipo "Professor de Cálculo 2".
+ * Herda da classe Inimigo.
+ */
 class ProfCalculo2 : public Inimigo {
-    int _ataque;
+    int _ataque; /**< Valor do ataque do Professor de Cálculo 2. */
+
 public:
+    /**
+     * @brief Construtor da classe ProfCalculo2.
+     * @param nome Nome do Professor de Cálculo 2.
+     */
     ProfCalculo2(string nome);
+
+    /**
+     * @brief Função que representa o ataque do Professor de Cálculo 2.
+     * @return Valor do ataque.
+     */
     int ataca() override;
+
+    /**
+     * @brief Função que representa a fala do Professor de Cálculo 2.
+     */
     void falar() override;
 };
 
-#endif 
+#endif
