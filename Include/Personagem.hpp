e1bce72e311ce87c4e45f27cc8788e3d52354d7a
vaahazz <143141341+vaahazz@users.noreply.github.com>
2023-12-04 21:32:48 -0300

    Update Personagem.hpp

--- Include/Personagem.hpp
+++ Include/Personagem.hpp
@@ -1,32 +1,70 @@
 #ifndef PERSONAGEM_HPP
 #define PERSONAGEM_HPP
+
 #include <iostream>
 #include "Item.hpp"
 #include <vector>
 
-using namespace std;
-
+/**
+ * @brief Classe para representar um Personagem.
+ */
 class Personagem {
-    string _nome;
-    int _vida = 100;
-    int _vida_maxima = 100;
-    int _ataque1 = 27;
-    int _ataque2 = 25;
-    std::vector<Item> inventario;
+    std::string _nome; /**< Nome do Personagem. */
+    int _vida; /**< Vida do Personagem. */
+    int _vida_maxima; /**< Vida máxima do Personagem. */
+    int _ataque1; /**< Ataque 1 do Personagem. */
+    int _ataque2; /**< Ataque 2 do Personagem. */
+    std::vector<Item> inventario; /**< Vetor de Itens no inventário do Personagem. */
 
 public:
-    Personagem(string nome);
+    /**
+     * @brief Construtor da classe Personagem.
+     * @param nome Nome do Personagem.
+     */
+    Personagem(std::string nome);
+
+    /**
+     * @brief Função para reduzir a vida do Personagem.
+     * @param dano Valor do dano a ser aplicado.
+     */
     void recebeDano(int dano);
-    int ataca(int tipo);
-    bool estaVivo();
-    void definirNome(string nome);
-    string getNome();
-    void addItem(Item item);
-    void usarItem(int indiceItem);
-    std::vector<Item>& getInventario();
-    void exibirInventario();
+
+    /**
+     * @brief Imprime informações do Personagem.
+     */
     void printInfo();
-    int getVida();
+
+    /**
+     * @brief Função para realizar um ataque com base no número escolhido.
+     * @param escolha Escolha do jogador (1 ou 2).
+     * @return Valor do dano causado.
+     */
+    int ataca(int escolha);
+
+    /**
+     * @brief Função para adicionar um Item ao inventário do Personagem.
+     * @param item Item a ser adicionado.
+     */
+    void adicionarItem(Item item);
+
+    /**
+     * @brief Função para usar um Item do inventário e regenerar vida.
+     * @param index Índice do Item a ser usado.
+     * @return Valor de regeneração de vida.
+     */
+    int usarItem(int index);
+
+    /**
+     * @brief Verifica se o Personagem está vivo.
+     * @return true se estiver vivo, false caso contrário.
+     */
+    bool estaVivo();
+
+    /**
+     * @brief Retorna o nome do Personagem.
+     * @return Nome do Personagem.
+     */
+    std::string getNome();
 };
 
 #endif
