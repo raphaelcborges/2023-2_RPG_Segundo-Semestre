517157f094b3bf3069065492f12cb6aea178c029
vaahazz <143141341+vaahazz@users.noreply.github.com>
2023-12-04 21:31:54 -0300

    Update Item.hpp

--- Include/Item.hpp
+++ Include/Item.hpp
@@ -1,18 +1,36 @@
 #ifndef ITEM_HPP
 #define ITEM_HPP
-#pragma once
 
 #include <string>
 
+/**
+ * @brief Classe para representar um Item.
+ */
 class Item {
 public:
+    /**
+     * @brief Construtor da classe Item.
+     * @param nome Nome do Item.
+     * @param valorDeRegeneracao Valor de regeneração de vida/NSG.
+     */
     Item(std::string nome, int valorDeRegeneracao);
+
+    /**
+     * @brief Retorna o nome do Item.
+     * @return Nome do Item.
+     */
     std::string getNome();
+
+    /**
+     * @brief Retorna o valor de regeneração do Item.
+     * @return Valor de regeneração.
+     */
     int getValorDeRegeneracao();
 
 private:
-    std::string nome;
-    int valorDeRegeneracao;
+    std::string nome; /**< Nome do Item. */
+    int valorDeRegeneracao; /**< Valor de regeneração de vida/NSG do Item. */
 };
 
 #endif
+
