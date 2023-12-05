f8498d4a8d19310701f0374017885692151dec4b
raphaelcborges <129281871+raphaelcborges@users.noreply.github.com>
2023-12-04 21:44:38 -0300

    Update ProfFundMec.hpp

--- Include/ProfFundMec.hpp
+++ Include/ProfFundMec.hpp
@@ -4,12 +4,30 @@
 #include "Inimigo.hpp"
 #include <iostream>
 
+/**
+ * @brief Classe que representa um Inimigo do tipo "Professor de Fundamentos Mecânicos".
+ * Herda da classe Inimigo.
+ */
 class ProfFundMec : public Inimigo {
-    int _ataque;
+    int _ataque; /**< Valor do ataque do Professor de Fundamentos Mecânicos. */
+
 public:
+    /**
+     * @brief Construtor da classe ProfFundMec.
+     * @param nome Nome do Professor de Fundamentos Mecânicos.
+     */
     ProfFundMec(string nome);
+
+    /**
+     * @brief Função que representa o ataque do Professor de Fundamentos Mecânicos.
+     * @return Valor do ataque.
+     */
     int ataca() override;
+
+    /**
+     * @brief Função que representa a fala do Professor de Fundamentos Mecânicos.
+     */
     void falar() override;
 };
 
-#endif 
+#endif
