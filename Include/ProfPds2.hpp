8cad1057033d4451920ff72150059fa4c2b3f485
vaahazz <143141341+vaahazz@users.noreply.github.com>
2023-12-04 21:33:28 -0300

    Update ProfPds2.hpp

--- Include/ProfPds2.hpp
+++ Include/ProfPds2.hpp
@@ -4,13 +4,30 @@
 #include "Inimigo.hpp"
 #include <iostream>
 
+/**
+ * @brief Classe que representa um Inimigo do tipo "Professor de Projeto e Desenvolvimento de Sistemas 2".
+ * Herda da classe Inimigo.
+ */
+class ProfPDS2 : public Inimigo {
+    int _ataque; /**< Valor do ataque do Professor de Projeto e Desenvolvimento de Sistemas 2. */
 
-class ProfPDS2: public Inimigo {
-    int _ataque;
 public:
+    /**
+     * @brief Construtor da classe ProfPDS2.
+     * @param nome Nome do Professor de Projeto e Desenvolvimento de Sistemas 2.
+     */
     ProfPDS2(string nome);
+
+    /**
+     * @brief Função que representa o ataque do Professor de Projeto e Desenvolvimento de Sistemas 2.
+     * @return Valor do ataque.
+     */
     int ataca() override;
+
+    /**
+     * @brief Função que representa a fala do Professor de Projeto e Desenvolvimento de Sistemas 2.
+     */
     void falar() override;
 };
 
-#endif 
+#endif
