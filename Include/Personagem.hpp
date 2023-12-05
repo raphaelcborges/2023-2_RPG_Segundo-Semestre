203ce130df8d33563ae79a09347eb0ce50c04b7c
kayquefb <149022324+kayquefb@users.noreply.github.com>
2023-12-05 00:00:44 -0300

    Update Personagem.hpp

--- Include/Personagem.hpp
+++ Include/Personagem.hpp
@@ -18,7 +18,9 @@
 using namespace std;
 
 /**
- * @brief Classe para representar um Personagem.
+ * @brief Classe para representar uma personagem no jogo
+ * @details Essa classe contém a implementação de um personagem em um jogo. Possui métodos para manipular
+ * a vida do personagem, realizar ataques, gerenciar o inventário e obter informações do personagem.
  */
 class Personagem {
     string _nome; /**< Nome do Personagem. */
@@ -29,65 +31,104 @@ class Personagem {
     std::vector<Item> inventario; /**< Vetor de Itens no inventário do Personagem. */
 
 public:
+
     /**
      * @brief Construtor da classe Personagem.
-     * @param nome Nome do Personagem.
+     * @details Este construtor inicializa um objeto Personagem com um nome fornecido. A vida é inicializada 
+     * em 100, assim como a vida máxima. Os valores de ataque também são inicializados, bem como o inventário é criado vazio.
+     * 
+     * @param nome 
      */
     Personagem(string nome);
+
     /**
-     * @brief Função para reduzir a vida do Personagem.
-     * @param dano Valor do dano a ser aplicado.
+     * @brief Faz o personagem receber dano
+     * @details Este método diminui a vida do personagem de acordo com a quantidade de dano recebido.
+     * 
+     * @param dano 
      */
     void recebeDano(int dano);
+
     /**
-     * @brief Função para realizar um ataque com base no número escolhido.
-     * @param escolha Escolha do jogador (1 ou 2).
-     * @return Valor do dano causado.
+     * @brief Faz o personagem atacar
+     * @details Este método retorna a quantidade de dano que o personagem causa, baseado no tipo de ataque escolhido.
+     * 
+     * @param tipo 
+     * @return int 
      */
     int ataca(int tipo);
+
     /**
-     * @brief Verifica se o Personagem está vivo.
-     * @return true se estiver vivo, false caso contrário.
+     * @brief Verifica se o personagem está vivo
+     * @details Este método verifica se o personagem ainda tem vida, ou seja, se a quantidade de vida dele 
+     * é maior do que zero.
+     * 
+     * @return true 
+     * @return false 
      */
     bool estaVivo();
+
     /**
-     * @brief Definir o nome do Personagem.
-     * @param nome Nome do Personagem.
+     * @brief Define o nome do personagem
+     * @details Este método recebe uma string e atribui esse valor ao nome do personagem.
+     * 
+     * @param nome 
      */
     void definirNome(string nome);
+
     /**
-     * @brief Retorna o nome do Personagem.
-     * @return Nome do Personagem.
+     * @brief Obtém o nome do personagem
+     * @details Este método retorna o nome atual do personagem.
+     * 
+     * @return string 
      */
     string getNome();
+
     /**
-     * @brief Função para adicionar um Item ao inventário do Personagem.
-     * @param item Item a ser adicionado.
+     * @brief Adiciona um item ao inventário do personagem
+     * @details Este método recebe um objeto do tipo Item e o adiciona ao vetor do inventário do personagem.
+     * 
+     * @param item 
      */
     void addItem(Item item);
+
     /**
-     * @brief Função para usar um Item do inventário e regenerar vida.
-     * @param index Índice do Item a ser usado.
-     * @return Valor de regeneração de vida.
+     * @brief Usa um item do inventário do personagem
+     * @details Este método recebe um índice que corresponde à posição de um item no vetor do inventário 
+     * e "usa" esse item, normalmente afetando algum estado do personagem.
+     * 
+     * @param indiceItem 
      */
     void usarItem(int indiceItem);
+
     /**
-     * @brief Exibe o vetor Inventário.
+     * @brief Obtém o inventário do personagem
+     * @details Este método retorna uma referência ao vetor do inventário do personagem, permitindo que se 
+     * tenha acesso aos itens que o personagem possui.
+     * 
+     * @return std::vector<Item>& 
      */
     std::vector<Item>& getInventario();
+
     /**
-     * @brief Exibir o Inventário do Personagem.
-     * @return Inventário.
+     * @brief Exibe o inventário do personagem
+     * @details Este método imprime na saída padrão todos os itens presentes no vetor do inventário do 
+     * personagem, geralmente com informações como o nome e o efeito de cada item.
      */
     void exibirInventario();
+
     /**
-     * @brief Exibir o NSG do Personagem.
-     * @return NSG.
+     * @brief Imprime as informações do personagem
+     * @details Este método imprime na saída padrão informações sobre o personagem, como o nome, a quantidade 
+     * de vida, o inventário, etc.
      */
     void printInfo();
+
     /**
-     * @brief Exibir a vida atual do Personagem.
-     * @return Vida atual.
+     * @brief Obtém a quantidade de vida do personagem
+     * @details Este método retorna a quantidade atual de vida do personagem.
+     * 
+     * @return int 
      */
     int getVida();
 };
