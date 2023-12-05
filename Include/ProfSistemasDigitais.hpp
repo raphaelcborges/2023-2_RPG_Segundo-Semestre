807da16a60eb7ca1a3d2bb756ae3616da76a41d6
raphaelcborges <129281871+raphaelcborges@users.noreply.github.com>
2023-12-04 21:45:08 -0300

    Update ProfSistemasDigitais.hpp

--- Include/ProfSistemasDigitais.hpp
+++ Include/ProfSistemasDigitais.hpp
@@ -4,13 +4,30 @@
 #include "Inimigo.hpp"
 #include <iostream>
 
-
+/**
+ * @brief Classe que representa um Inimigo do tipo "Professor de Sistemas Digitais".
+ * Herda da classe Inimigo.
+ */
 class ProfSD : public Inimigo {
-    int _ataque;
+    int _ataque; /**< Valor do ataque do Professor de Sistemas Digitais. */
+
 public:
+    /**
+     * @brief Construtor da classe ProfSD.
+     * @param nome Nome do Professor de Sistemas Digitais.
+     */
     ProfSD(string nome);
+
+    /**
+     * @brief Função que representa o ataque do Professor de Sistemas Digitais.
+     * @return Valor do ataque.
+     */
     int ataca() override;
+
+    /**
+     * @brief Função que representa a fala do Professor de Sistemas Digitais.
+     */
     void falar() override;
 };
 
-#endif 
+#endif
