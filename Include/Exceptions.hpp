0fa5f15dfbc43262cf5d38bbd8b0ec32bd3074ab
kayquefb <149022324+kayquefb@users.noreply.github.com>
2023-12-04 22:56:41 -0300

    Update Exceptions.hpp

--- Include/Exceptions.hpp
+++ Include/Exceptions.hpp
@@ -1,27 +1,21 @@
+Batalha.hpp
 #include <exception>
 
 namespace exceptions {
 
-    /**
-     * @brief Exceção para número inválido.
-     */
     class numero_invalido : public std::exception {
     public:
 
         const char* what() const throw() {
-            return "Parece que você ainda não aprendeu os números direitinho, escolha um número válido";
+            return "Parece que voce ainda nao aprendeu os numeros direitinho, escolha um numero valido";
         }
     };
 
-    /**
-     * @brief Exceção para letra inválida.
-     */
     class letra_invalida : public std::exception {
     public:
 
         const char* what() const throw() {
-            return "Ops, parece que você não sabe a diferença entre números e letras, tudo bem! Tente de novo (número)\n";
+            return "Ops, parece que voce nao sabe a diferenca entre numeros e letras, tudo bem! Tente de novo (numero)\n";
         }
     };
 }
-
