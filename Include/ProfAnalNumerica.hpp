30723755501c56eed0ef66fd9ca28d17a3ceeea7
raphaelcborges <129281871+raphaelcborges@users.noreply.github.com>
2023-12-04 22:54:09 -0300

    Update ProfAnalNumerica.hpp

--- Include/ProfAnalNumerica.hpp
+++ Include/ProfAnalNumerica.hpp
@@ -1,3 +1,4 @@
+
 #ifndef PROFANALNUMERICA_HPP
 #define PROFANALNUMERICA_HPP
 
@@ -8,17 +9,17 @@
  * @brief Classe que representa um Inimigo do tipo "Professor de Análise Numérica".
  * Herda da classe Inimigo.
  */
+
 class ProfAnalNumerica : public Inimigo {
     int _ataque; /**< Valor do ataque do Professor de Análise Numérica. */
-
 public:
     /**
      * @brief Construtor da classe ProfAnalNumerica.
      * @param nome Nome do Professor de Análise Numérica.
      */
     ProfAnalNumerica(string nome);
-
-    /**
+    
+/**
      * @brief Função que representa o ataque do Professor de Análise Numérica.
      * @return Valor do ataque.
      */
@@ -30,4 +31,4 @@ public:
     void falar() override;
 };
 
-#endif
+#endif 
