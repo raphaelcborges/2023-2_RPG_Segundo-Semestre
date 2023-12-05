439e89cd6b0fe058d7dc8518500bddac8c2118ed
kayquefb <149022324+kayquefb@users.noreply.github.com>
2023-12-05 00:17:46 -0300

    Update Batalha.cpp

--- src/Batalha.cpp
+++ src/Batalha.cpp
@@ -1,18 +1,42 @@
-#ifndef BATALHA_HPP
-#define BATALHA_HPP
+#include <iostream>
+#include "Batalha.hpp"
 
-#include "Personagem.hpp"
-#include "Inimigo.hpp"
+Batalha::Batalha(Personagem* personagem, Inimigo* inimigo) : personagem(personagem), inimigo(inimigo) {}
 
-class Batalha {
-    Personagem* personagem;
-    Inimigo* inimigo;
+void Batalha::executaTurno(int escolha) {
+    int dano = personagem->ataca(escolha);
+    inimigo->recebeDano(dano);
+    std::cout << "Voce foi um bom aluno e a aula do professor ficou " << dano << " vezes mais facil !\n" << std::endl;
 
-public:
-    Batalha(Personagem* personagem, Inimigo* inimigo);
-    void executaTurno(int escolha);
-    bool terminou();
-    void executaTurnoInimigo();
-};
+    if (!inimigo->estaVivo()) {
+        personagem->printInfo();
+        inimigo->printInfo();
+        std::cout << "Voce passou nessa materia!\n" << std::endl;
+        return;
+    }
 
-#endif
+    dano = inimigo->ataca();
+    inimigo->falar();
+    personagem->recebeDano(dano);
+    std::cout << "Mas o professor nao facilitou e voce perdeu " << dano << " de NSG!\n" << std::endl;
+
+    if (!personagem->estaVivo()) {
+        personagem->printInfo();
+        inimigo->printInfo();
+    }
+}
+
+bool Batalha::terminou() {
+    return !personagem->estaVivo() || !inimigo->estaVivo();
+}
+
+void Batalha::executaTurnoInimigo() {
+    if (!personagem->estaVivo() || !inimigo->estaVivo()) {
+        return;
+    }
+
+    int ataqueInimigo = inimigo->ataca();
+    personagem->recebeDano(ataqueInimigo);
+    inimigo->falar();
+    cout << "O professor nao foi legal e voce perdeu " << ataqueInimigo << " de NSG" << endl;
+}
